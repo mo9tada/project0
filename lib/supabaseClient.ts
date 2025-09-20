6e038dc60e0af1171ce9f103f0d1b42e0b8c3cc5
Moktada Zaidi <moktadazaidi@gmail.com>
2025-09-20 22:09:44 +0100

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,10 +1,8 @@
-// supabaseClient.js
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  const supabaseUrl=process.env.NEXT_PUBLIC_SUPABASE_URL,
-  const supabaseKey=process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
 // Server-side client for server actions
 export const createServerClient = () => {
